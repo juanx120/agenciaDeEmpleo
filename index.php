c51ef3ce8a49250393d38e9b50a9cd40948f1abe
Maria Paula Castrillon <mapaucastrillon@gmail.com>
2023-05-10 13:07:59 -0500

    actu_index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/inicio.css">
+    <title>Agencia Empleos Bogota</title>
+</head>
+
+<body id="backgroundI">
+    <div class="cuadro">
+        <form action="login" id="login">
+            <label for="fname">Correo:</label>
+            <input type="text" id="email" name="fname"><br><br>
+            <label for="lname">Contraseña:</label>
+            <input type="text" id="password" name="lname"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+</body>
