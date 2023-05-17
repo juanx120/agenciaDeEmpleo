5591a9b740d5de5712c9a8e84a80b0adaca8d1bd
juanx120 <guerrerojuancho78@gmail.com>
2023-05-17 10:16:23 -0500

    Quite comentarios

--- conection.php
+++ conection.php
@@ -19,16 +19,16 @@ else{
 
 $resultado = sqlsrv_query($conn, "SELECT * FROM [dbo].[Usuario]");
 // O usando el estilo orientado a objetos
-//$resultado = $conn->query("SELECT * from [dbo].[usuario]");
+$resultado = $conn->query("SELECT * from [dbo].[usuario]");
 // Verificar si la consulta fue exitosa
-//if ($resultado) {
+if ($resultado) {
   // Hacer algo con el resultado, por ejemplo mostrar los datos
   while ($fila = sqlsrv_fetch_object($resultado)) {
     echo $fila->IdUsuario , " " , $fila->Correo , "<br>";
   }
-//} else {
+} else {
   // Mostrar un mensaje de error
   echo "La consulta falló: " , sqlsrv_error($conn);
-//}
+}
 
 ?>
