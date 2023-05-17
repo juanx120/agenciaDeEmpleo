140d6aa36a58a445ef30865f89f07a4998a87e38
Maria Paula Castrillon <mapaucastrillon@gmail.com>
2023-05-16 22:44:40 -0500

    coreccion loing

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
     <link rel="stylesheet"  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" >
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/index.css">
     <link rel="stylesheet" href="/css/inicio.css">
     <title>Agencia Empleos Bogota</title>
 </head>
