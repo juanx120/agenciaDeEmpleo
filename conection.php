93068591d2d3c9f66a7572b738eca3d546b04fbc
juanx120 <guerrerojuancho78@gmail.com>
2023-05-17 11:24:15 -0500

    Prueba inicio sesion1

--- conection.php
+++ conection.php
@@ -15,24 +15,4 @@ else{
     echo("Sirvio, perros XDXDXDX");
 }
 
-
-
-$resultado = sqlsrv_query($conn, "SELECT * FROM [dbo].[Usuario]");
-// O usando el estilo orientado a objetos
-//$resultado = $conn->query("SELECT * FROM [dbo].[usuario]");
-// Verificar si la consulta fue exitosa
-if ($resultado) {
-  // Hacer algo con el resultado, por ejemplo mostrar los datos
-  while ($fila = sqlsrv_fetch_object($resultado)) {
-    echo $fila->IdUsuario , " " , $fila->Correo , "<br>";
-  }
-} else {
-  // Mostrar un mensaje de error
-  echo "La consulta falló: " , sqlsrv_error($conn);
-}
-
 ?>
-
-<script>
-  alert( "<?php echo $conn; ?>" );
-</script>
