8fd31c1107b2af11bc7f0b0b7ea741b8d3795107
juanx120 <guerrerojuancho78@gmail.com>
2023-05-18 18:11:05 -0500

    Se agrego un echo para comprobar

--- inicio.php
+++ inicio.php
@@ -1,6 +1,7 @@
 <?php include('menu.php') ;
 session_start();
  $Idu = $_SESSION['Idu'];
+ echo $_SESSION['Idu'];
  echo '<script>'
  echo 'console.log ("el valor de usuario es:", <?php $Idu ?'
  echo '</script>'
