7cc54777f077e2eb678cbd64dafc21b10487f773
juanx120 <guerrerojuancho78@gmail.com>
2023-05-18 22:20:28 -0500

    Se agrego otra varible en el console log

--- inicio.php
+++ inicio.php
@@ -4,7 +4,7 @@ include('conection.php');
  //$Idu = $_SESSION['Idu'];
  echo $_SESSION['Idu'];
  echo '<script>';
- echo 'console.log ("el valor de usuario es:"', $_GET['Idu'];
+ echo 'console.log ("el valor de usuario es:"', $_GET['Idu'], $_SESSION['Idu'];
  echo ')</script>';
  ?>
 
