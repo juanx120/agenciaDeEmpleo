52725082eace571180de0433ce0f7ccfc7b6441c
juanx120 <guerrerojuancho78@gmail.com>
2023-05-21 18:57:40 -0500

    Y se me olvido un ;

--- inicio.php
+++ inicio.php
@@ -6,7 +6,7 @@ include('session.php');
  echo $_SESSION['Idu'];
  echo '<script>';
  echo 'console.log ("el valor de usuario es:"', $_SESSION['Idu'];
- echo ')'
+ echo ')';
  echo 'console.log ("el valor de usuario es:"', $_GET['Idu'], $_SESSION['Idu'];
  echo ')</script>';
  ?>
