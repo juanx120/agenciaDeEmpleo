344bb6772612d451d29e83726f2312c6b3b1c8e6
Maria Paula Castrillon <mapaucastrillon@gmail.com>
2023-05-22 17:28:22 -0500

    dt usu

--- inicio.php
+++ inicio.php
@@ -43,23 +43,6 @@
             <label for="telefono">Teléfono:</label>
             <input type="text" name="telefono" class="txtformulario">
         </div>   
-    
-        <div class="content">
-                <div class="circulo"><span class="material-symbols-outlined">person</span></div>
-                <input type="text" name="CorreoIngreso" class="txtingreso" placeholder="Correo"><br><br>
-            </div>
-            <div class="content">
-                <div class="circulo"><span class="material-symbols-outlined">lock</span></div>
-            <input type="text" name="ClaveIngreso" class="txtingreso" placeholder="Contraseña"><br><br>
-            </div> 
-            <input type="submit" name="btningreso" value="INGRESAR" class="button1">
-            <div class="opciones">
-                <a id="btn-password" class="btn-registro">¿Has olvidado tu clave?</a>
-            </div>
-            <div class="opciones">
-                <p class="btn-registro">¿No tienes cuenta?</p>
-                <a id="btn-registro" class="btn-registro">Regristrate</a>
-            </div>
     </form>
     
 
