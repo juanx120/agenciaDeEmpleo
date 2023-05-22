f4813354009a689a5631714aa9ce1e2babd9fd25
juanx120 <guerrerojuancho78@gmail.com>
2023-05-21 21:40:41 -0500

    Modal de regitro

--- js/registro.js
+++ js/registro.js
@@ -1,5 +1,6 @@
 const btnAbrirR = document.querySelector("#btn-registro")
 const btnCerrarR = document.querySelector("#btn-cerrar-modalR")
+const modalR = document.querySelector("#modalr")
 
 btnAbrirR.addEventListener("click", () => {
     modalR.showModal();
