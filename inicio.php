9a1badad08c547f9473814d76914cf7a4b9c0764
Maria Paula Castrillon <mapaucastrillon@gmail.com>
2023-05-22 18:43:28 -0500

    front tabla usuario

--- inicio.php
+++ inicio.php
@@ -9,54 +9,50 @@
     echo ')</script>';
 ?>
 
-
     <div class = "contenido"> 
-    <form id="dt-aspirante" class='info' method="post">
-        <div class="ttlcuadro">
-            <div class="titulo">
-                <h1>Datos personales</h1>
-            </div>
-        </div>  
-            <h3> Principales </h3> 
-            <label for="nombre">Nombres:</label>
-            <input type="text" name="nombre" class="txtformulario">
-            <label for="apellido">Apellidos:</label>
-            <input type="text" name="apellido" class="txtformulario">
-            <label for="identificacion">No. Identificación:</label>
-            <input type="text" name="identificacion" class="txtformulario">
-            <label for="genero">Genero</label>
-            <input type="text" name="genero" class="txtformulario">
-            <label for="estcivil">Estado civil</label>
-            <input type="text" name="estcivil" class="txtformulario">
-            <h3> Nacimiento </h3> 
-            <label for="lugarnc">Lugar:</label>
-            <?php
-            $resultado = sqlsrv_query($conn, "SELECT * FROM [dbo].[Paises]");
-            echo '<select name="lugarnc">';
-            while ($fila = sqlsrv_fetch_object($resultado)) {
-                    echo '<option class="txtformulario" value="' , $fila->IdPais , '">' , $fila->Pais , '</option>';
-                }
-            echo '</select>'
-            ?>
-            <label for="fechanc">Fecha:</label>
-            <input type="date" name="fechanc" class="txtformulario">
-            <h3> Ubicación </h3> 
-            <label for="ciudad">Ciudad:</label>
-            <input type="text" name="ciudad" class="txtformulario">
-            <label for="pais">País:</label>
-            <input type="text" name="pais" class="txtformulario">
-            <label for="direccion">Dirección:</label>
-            <input type="text" name="direccion" class="txtformulario">
-            <h3> Contacto </h3> 
-            <label for="telefono">Teléfono:</label>
-            <input type="number" name="telefono" class="txtformulario">
-         
-    </form>
-    
-
-
-
-
+        <form id="dt-aspirante" class='info' method="post">
+            <div class="ttlcuadro">
+                <div class="titulo">
+                    <h2>Datos personales</h2>
+                </div>
+            </div>  
+                <h3> Principales </h3> 
+                <label for="nombre">Nombres:</label>
+                <input type="text" name="nombre" class="txtformulario">
+                <label for="apellido">Apellidos:</label>
+                <input type="text" name="apellido" class="txtformulario">
+                <label for="identificacion">No. Identificación:</label>
+                <input type="text" name="identificacion" class="txtformulario">
+                <label for="genero">Genero</label>
+                <input type="text" name="genero" class="txtformulario">
+                <label for="estcivil">Estado civil</label>
+                <input type="text" name="estcivil" class="txtformulario">
+                <hr>
+                <h3> Nacimiento </h3> 
+                <label for="lugarnc">Lugar:</label>
+                <?php
+                $resultado = sqlsrv_query($conn, "SELECT * FROM [dbo].[Paises]");
+                echo '<select name="lugarnc">';
+                while ($fila = sqlsrv_fetch_object($resultado)) {
+                        echo '<option class="txtformulario" value="' , $fila->IdPais , '">' , $fila->Pais , '</option>';
+                    }
+                echo '</select>'
+                ?>
+                <label for="fechanc">Fecha:</label>
+                <input type="date" name="fechanc" class="txtformulario">
+                <hr>
+                <h3> Ubicación </h3> 
+                <label for="ciudad">Ciudad:</label>
+                <input type="text" name="ciudad" class="txtformulario">
+                <label for="pais">País:</label>
+                <input type="text" name="pais" class="txtformulario">
+                <label for="direccion">Dirección:</label>
+                <input type="text" name="direccion" class="txtformulario">
+                <hr>
+                <h3> Contacto </h3> 
+                <label for="telefono">Teléfono:</label>
+                <input type="number" name="telefono" class="txtformulario">
+        </form>
 
     </div>
 </body>
