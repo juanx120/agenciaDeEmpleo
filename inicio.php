92c4cbbbbbceb109ef87944cf7037a778c34a432
juanx120 <guerrerojuancho78@gmail.com>
2023-05-22 23:17:30 -0500

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -9,7 +9,7 @@
     echo ')</script>';
 ?>
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" integrity="sha512-nMNlpuaDPrqlEls3IX/Q56H36qvBASwb3ipuo3MxeWbsQB1881ox0cRv7UPTgBlriqoynt35KjEwgGUeUXIPnw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <div class = "contenido"> 
         <form id="dt-aspirante" class='info' method="post">
             <div class="ttlcuadro">
