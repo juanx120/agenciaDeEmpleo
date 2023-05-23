47114c9c9be2eff8410033ee96802281c9b67095
juanx120 <guerrerojuancho78@gmail.com>
2023-05-23 00:00:12 -0500

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -5,6 +5,8 @@
     echo '<script>';
     echo 'console.log ("el valor de usuario es:', $_SESSION['Idu'];
     echo '")';
+    echo '</script>';
+    echo '<script>'
     echo 'console.log ("el valor de usuario es:', $_GET['Idu'], $_SESSION['Idu'];
     echo '")</script>';
 ?>
