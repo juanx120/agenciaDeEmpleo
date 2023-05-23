48e089263539aa28f7e8296f15744c24f749c9d1
juanx120 <guerrerojuancho78@gmail.com>
2023-05-22 23:14:37 -0500

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -9,6 +9,7 @@
     echo ')</script>';
 ?>
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" integrity="sha512-nMNlpuaDPrqlEls3IX/Q56H36qvBASwb3ipuo3MxeWbsQB1881ox0cRv7UPTgBlriqoynt35KjEwgGUeUXIPnw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <div class = "contenido"> 
         <form id="dt-aspirante" class='info' method="post">
             <div class="ttlcuadro">
@@ -174,11 +175,11 @@
     }
     ?>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.14/js/select2.min.js"></script>
-<script>
-  $(document).ready(function() {
-    $('#selectorPro').select2();
-  });
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js" integrity="sha512-2ImtlRlf2VVmiGZsjm9bEyhjGW4dU7B6TNwh/hx/iSByxNENtj3WVE6o/9Lj4TJeVXPi4bnOIMXFIJJAeufa0A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+        $(document).ready(function() {
+        $('#selectorPro').select2();
+        });
 </script>
 
 </body>
