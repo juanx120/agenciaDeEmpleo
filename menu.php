ae0ab165153bd59983458f90c82384e5ef632978
Maria Paula Castrillon <mapaucastrillon@gmail.com>
2023-05-24 07:57:17 -0500

    ajuste menu

--- menu.php
+++ menu.php
@@ -2,12 +2,12 @@
 <div class=contenedor>
     <div id='menu'>
         <a href='inicio.php'>Datos usuario</a>
-        <?php if($TaUser->TipoUsuario == 'P' OR $TaUser->TipoUsuario == 'A'){ ?> 
+        <?php if($TaUser->TipoUsuario == 'P' || $TaUser->TipoUsuario == 'A'){ ?> 
         <a href='hoja.php'>Hoja de vida</a>
         <a href='#'>Vacantes</a>
         <a href='empresas.php'>Empresas</a>
         <?php } ?>
-        <?php if($TaUser->TipoUsuario == 'E' OR $TaUser->TipoUsuario == 'A'){ ?> 
+        <?php if($TaUser->TipoUsuario == 'E' || $TaUser->TipoUsuario == 'A'){ ?> 
         <a href='aspirantes.php'>Aspirantes</a>
         <a href='#'>Crear vacante</a>
         <a href='#'>Reportes</a>
