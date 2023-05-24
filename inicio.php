c4236afb9b9eafb82103a9b36377bef924603955
juanx120 <guerrerojuancho78@gmail.com>
2023-05-23 21:23:41 -0500

    Prueba del session

--- inicio.php
+++ inicio.php
@@ -2,9 +2,9 @@
 
     //$Idu = $_SESSION['Idu'];
     //echo $_SESSION['Idu'];
-    //echo '<script>';
-    //echo 'console.log ("el valor de usuario es:', $_SESSION['Idu'];
-    //echo '")';
+    echo '<script>';
+    echo 'console.log ("el valor de usuario es:', $_SESSION['Idu'];
+    echo '")';
     //echo '</script>';
     //echo '<script>'
     //echo 'console.log ("el valor de usuario es:', $_GET['Idu'], $_SESSION['Idu'];
