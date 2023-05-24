4c3d3970f96231d3340b3907b11a020e4064f2ec
Maria Paula Castrillon <mapaucastrillon@gmail.com>
2023-05-23 23:38:19 -0500

    Correcion tabla estudio HV

--- hoja.php
+++ hoja.php
@@ -44,7 +44,7 @@
                     $resultado = sqlsrv_query( $conn, $sql, $params);
                     while ($fila = sqlsrv_fetch_object($resultado)) {
                         echo "<tr class='espacio'></tr>";
-                        echo "<tr class='row'> <td>$fila->Empresa<td><td>$fila->PuestoOcupado<td><td>$fila->Ano<td> <td>$fila->Descipcion<td> </tr>";
+                        echo "<tr class='row'> <td>$fila->Empresa</td><td>$fila->PuestoOcupado</td><td>$fila->Ano</td> <td>$fila->Descipcion</td> </tr>";
                     }
                 ?>
             </tbody>
