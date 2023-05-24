045c8980ff2aeddb2728548dae9647a385b346c1
juanx120 <guerrerojuancho78@gmail.com>
2023-05-24 11:32:01 -0500

    Update vacante.php

--- vacante.php
+++ vacante.php
@@ -31,9 +31,10 @@
                     INNER JOIN VacanteXEmpresa c on IdVacante = c.Vacante INNER JOIN Empresa on NIT = c.Empresa";
                     $resultado = sqlsrv_query( $conn, $sql);
                     while ($fila = sqlsrv_fetch_array($resultado)) {
+                        $fechaFin = $fila['FechaFin']->format('Y/m/d');
                         echo "<tr class='espacio'></tr>";
                         echo "<tr class='row_HV'> <td>".$fila['Titulo']."</td><td>".$fila['RazonSocial']."</td><td>".$fila['Profesion']."</td>
-                        <td>".$fila['FechaFin']."</td></tr>";
+                        <td>".$fechaFin."</td></tr>";
                     }
                 ?>
         </tbody>
